0a6ebe0f20ec7ed9998585a119b3964ff53374c8
Laura Mas <lauramas@mail.fresnostate.edu>
2026-04-11 18:47:56 -0700

    Implemented Policy 5 (Dealer-aware Strategy)

--- policies.py
+++ policies.py
@@ -1,11 +1,11 @@
 # policies.py --> Defines policies
 
 # Policy 1: if hand >= 17 stick, else hit
-def policy_1(hand):
+def policy_1(hand, dealer_upcard=None):
     return hand.get_value() < 17
 
 # Policy 2: if hand >= 17 AND hard --> stick, else hit (unless hand = 21)
-def policy_2(hand):
+def policy_2(hand, dealer_upcard=None):
     value = hand.get_value()
     if value == 21: return False
 
@@ -15,20 +15,42 @@ def policy_2(hand):
     return True
 
 # Policy 3: always stick
-def policy_3(hand):
+def policy_3(hand, dealer_upcard=None):
     return False
 
 # Policy 4 (Custom 1): Soft-hand agressive strategy
-def policy_4(hand):
+def policy_4(hand, dealer_upcard=None):
     if hand.is_soft():
         return hand.get_value() < 19
     return hand.get_value() < 17
 
-# Policy 5 (Custom 2): Risk-averse (avoids bust)
-def policy_5(hand):
+# Policy 5 (Custom 2): Dealer-aware Strategy
+def policy_5(hand, dealer_upcard):
     value = hand.get_value()
+
+    dealer_value = dealer_upcard
+    if dealer_upcard == 1:
+        dealer_value = 11
+    
     if value >= 17:
         return False
-    if value == 16:
-        return False
-    return True
+    
+    if hand.is_soft():
+        if value >= 19:
+            return False
+        
+        if dealer_value in [2, 3, 4, 5, 6]:
+            return value < 18
+        
+        return True
+    
+    if value <= 11:
+        return True
+    
+    if 12 <= value <= 16:
+        if dealer_value in [2, 3, 4, 5, 6]:
+            return False
+        else:
+            return True
+        
+    return False
