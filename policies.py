70ff73c85dcbc4ae5ab2972fd1e8a8ab74d146f2
Laura Mas <lauramas@mail.fresnostate.edu>
2026-04-11 17:31:11 -0700

    Added policy 3 and 2 custom policies

--- policies.py
+++ policies.py
@@ -1,7 +1,6 @@
 # policies.py --> Defines policies
 
 # Policy 1: if hand >= 17 stick, else hit
-
 def policy_1(hand):
     return hand.get_value() < 17
 
@@ -13,4 +12,23 @@ def policy_2(hand):
     if value >= 17 and not hand.is_soft():
         return False
     
+    return True
+
+# Policy 3: always stick
+def policy_3(hand):
+    return False
+
+# Policy 4 (Custom 1): Soft-hand agressive strategy
+def policy_4(hand):
+    if hand.is_soft():
+        return hand.get_value() < 19
+    return hand.get_value() < 17
+
+# Policy 5 (Custom 2): Risk-averse (avoids bust)
+def policy_5(hand):
+    value = hand.get_value()
+    if value >= 17:
+        return False
+    if value == 16:
+        return False
     return True
